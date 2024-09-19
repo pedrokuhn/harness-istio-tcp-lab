7e0f9317dc8b1b89d10b51d427b785a0909ce6d6
Pedro Mastelaro <phmastelaro@gmail.com>
2024-09-19 14:07:41 -0700

    Adding/Fixing file

--- helper/check.sh
+++ helper/check.sh
@@ -1,8 +1,3 @@
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-export TCP_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="tcp")].port}')
 
-
-for i in {1..10}; do \
-  kubectl exec "$(kubectl get pod -l app=sleep -n pedro-staging -o jsonpath={.items..metadata.name})" \
-    -c sleep -n pedro-staging -- sh -c "(date; sleep 1) | nc $INGRESS_HOST $TCP_INGRESS_PORT"; \
-done
+for i in {1..10}; do curl http://$INGRESS_HOST/anything; done
